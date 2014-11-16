9c853cdbc260510364d62c0d80d27181f552f2c6
Bernix01 <gbernal096@hotmail.com>
2014-11-15 23:43:20 -0500

    no changes at all

--- app/src/main/java/nimbus/ec/nap/MainActivity.java
+++ app/src/main/java/nimbus/ec/nap/MainActivity.java
@@ -1,10 +1,11 @@
 package nimbus.ec.nap;
 
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,8 +13,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
+        setSupportActionBar(toolbar);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
